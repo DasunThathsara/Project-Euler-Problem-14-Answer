ec9437aca15f8e8aebe1edd8c267d5eeecfa3fbd
Dasun Thathsara <81586965+DasunThathsara@users.noreply.github.com>
2021-05-20 14:01:02 +0530

    Create P14.py

---
+++ P14.py
@@ -0,0 +1,16 @@
+t=0
+for n in range(1,1000000):
+    l=[n]
+    while True:
+        if n%2==0:
+            x=n/2
+        else:
+            x=3*n+1
+        l.append(x)
+        n=x
+        if n==1:
+            break
+    if t<len(l):
+        t=len(l)
+        w=l
+print(len(w))
